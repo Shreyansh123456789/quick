4f70979a5928031538ab90e0aa7fc98dc3a3c631
Shreyansh123456789 <155379681+Shreyansh123456789@users.noreply.github.com>
2025-05-01 13:52:30 +0530

    Update icons.js

--- src/icons.js
+++ src/icons.js
@@ -22,4 +22,19 @@ const icons = {
   Canceled: <FcCancel size={20} />, // Canceled status
 };
 
+// Define a collection of icons for different priorities and statuses
+const icons = {
+  0: <BsThreeDots size={20} color='green' />, // Priority 0
+  1: <MdSignalCellularAlt1Bar size={20} />, // Priority 1
+  2: <MdSignalCellularAlt2Bar size={20} />, // Priority 2
+  3: <MdSignalCellularAlt size={20} />, // Priority 3
+  4: <RiErrorWarningFill size={20} color='orange' />, // Priority 4
+  Backlog: <FcOvertime size={20} />, // Backlog status
+  Todo: <LuListTodo size={20} />, // Todo status
+  'In progress': <FcProcess size={20} />, // In progress status
+  Done: <BsFillCheckCircleFill size={20} color='green' />, // Done status
+  Canceled: <FcCancel size={20} />, // Canceled status
+};
+
+
 export default icons;
